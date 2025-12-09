e6d916ee692133b68d6166efebc988517a77cecc
massahito <110250805+massahito@users.noreply.github.com>
2025-12-09 15:48:00 +0100

    feat: Run TaskServer in the daemon by default. #36
    As Is
        - running TaskServer in the foreground by default
    
    To Be
        - running TaskServer in the foreground by default
        - add option -n to run TaskServer in the foreground

--- log.go
+++ log.go
@@ -8,6 +8,7 @@ import (
 func SetLogger(log Log) error {
 	file, err := os.OpenFile(log.Path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
 	if err != nil {
+		slog.Error("SetLogger", "error", err.Error())
 		return err
 	}
 
--- server/taskserver/main.go
+++ server/taskserver/main.go
@@ -1,20 +1,61 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 
+	t "github.com/massahito/taskmaster"
 	s "github.com/massahito/taskmaster/server"
+	"github.com/sevlyar/go-daemon"
 )
 
 func main() {
 	var cFlag = flag.String("c", "./taskmaster.yaml", "specify configuration files. Default: ./taskmaster.yaml")
+	var nFlag = flag.Bool("n", false, "run taskserver in the foreground.")
 	flag.Parse()
 
-	err := s.Run(*cFlag)
+	// configuration check
+	cfg, err := t.Parse(*cFlag)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 		os.Exit(1)
 	}
+
+	// socket path check
+	if err := checkFileExist(cfg.Socket.Path); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+
+	// daemonize
+	if !*nFlag {
+		cntxt := &daemon.Context{}
+		d, err := cntxt.Reborn()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+			os.Exit(1)
+		}
+		if d != nil {
+			return
+		}
+		defer cntxt.Release()
+	}
+
+	// create server
+	err = s.Run(*cFlag)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+func checkFileExist(path string) error {
+	var err error
+	if _, err = os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+
+	return fmt.Errorf("Error: Another program is already listening on a port that one of our HTTP servers is configured or the file already exists.")
 }
